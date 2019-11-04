f66b911309a251dcd8c57c841ba817944ebbfd33
Sweetjy <47705757+Sweetjy@users.noreply.github.com>
2019-11-04 20:55:33 +0800

    Update sessions_controller.rb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
   include SessionsHelper
 
-  def create
+  def create #post
     user = User.find_by(email: login_params[:email].downcase)
     if user && user.authenticate(login_params[:password])
       log_in user
@@ -10,11 +10,29 @@ class SessionsController < ApplicationController
     else
       flash= {:danger => '账号或密码错误'}
     end
-    redirect_to root_url, :flash => flash
+      redirect_to courses_url, :flash => flash  #老师和学生
+      # redirect_to root_url, :flash => flash
   end
+  
+  def new #get
 
-  def new
-
+  end
+  
+  def admincreate #post
+    user = User.find_by(email: login_params[:email].downcase)
+    if user && user.authenticate(login_params[:password])
+      log_in user
+      params[:session][:remember_me] == '1' ? remember_user(user) : forget_user(user)
+      flash= {:info => "欢迎回来: #{user.name} :)"}
+    else
+      flash= {:danger => '账号或密码错误'}
+    end
+      redirect_to rails_admin_url, :flash => flash  #管理员
+      # redirect_to root_url, :flash => flash
+  end
+  
+  def admin #get
+    
   end
 
   def destroy
